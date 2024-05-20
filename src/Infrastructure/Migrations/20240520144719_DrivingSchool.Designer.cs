0f7e152eb5ee6b9ffb92687f1547ebf2dd6b7c16
Romain <romain.reydel@gmail.com>
2024-05-20 16:48:41 +0200

    fix : DrivingSchool database migration

--- src/Infrastructure/Migrations/20240520144719_DrivingSchool.Designer.cs
+++ src/Infrastructure/Migrations/20240520144719_DrivingSchool.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240520144719_DrivingSchool")]
+    partial class DrivingSchool
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
